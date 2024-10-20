80180d1fced462e69c0d15df842ae6423a4206aa
Ukhatov Alexander <alexander.ukhatov@gmail.com>
2024-10-20 21:16:54 +0200

    removed unused in FE

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { RouterProvider, createRouter } from "@tanstack/react-router"
-import ReactDOM from "react-dom/client"
-import { routeTree } from "./routeTree.gen"
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
 
-import { StrictMode } from "react"
-import { OpenAPI } from "./client"
-import theme from "./theme"
+import { StrictMode } from "react";
+import theme from "./theme";
 
-OpenAPI.BASE = import.meta.env.VITE_API_URL
-OpenAPI.TOKEN = async () => {
-  return localStorage.getItem("access_token") || ""
-}
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
@@ -29,5 +23,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
