24f376ec3533cd6428c79edf2e9ac5e37c28b587
Ukhatov Alexander <alexander.ukhatov@gmail.com>
2025-02-03 00:35:11 +0100

    added mobile

--- frontend/src/components/main/HeaderSection.tsx
+++ frontend/src/components/main/HeaderSection.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "../../hooks/useTranslation";
 import { LanguageSwitcher } from "../translation/LanguageSwitcher";
+import { Menu, X } from "lucide-react";
 
 const HeaderSection = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
   const t = useTranslation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+      if (window.innerWidth >= 768) setIsMenuOpen(false); // Close menu on resize to larger screen
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
@@ -29,19 +48,21 @@ const HeaderSection = () => {
       {/* <Text fontSize="50" color="#3399ff" opacity="50%">
             Yoga Open Soul
           </Text> */}
-      <button
-        onClick={() => scrollToSection(section_id)}
-        style={{
-          color: "gray",
-          fontWeight: "500",
-          transition: "color 0.2s ease-in-out",
-          //   "&:hover": {
-          //     color: "black",
-          //   },
-        }}
-      >
-        {title}
-      </button>
+      {!isMobile && (
+        <button
+          onClick={() => scrollToSection(section_id)}
+          style={{
+            color: "gray",
+            fontWeight: "500",
+            transition: "color 0.2s ease-in-out",
+            //   "&:hover": {
+            //     color: "black",
+            //   },
+          }}
+        >
+          {title}
+        </button>
+      )}
       <h2
         style={{
           fontSize: "4rem",
@@ -52,9 +73,25 @@ const HeaderSection = () => {
       >
         {/* Yoga Open Soul */}
         {/* {language} */}
-        {t.nav.about}
+        {/* {t.nav.about} */}
       </h2>
       <LanguageSwitcher />
+      {isMobile && (
+        <button
+          style={{
+            background: "none",
+            border: "none",
+            color: "black",
+            cursor: "pointer",
+            padding: "8px",
+            minWidth: "20px",
+          }}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          aria-label="Toggle Menu"
+        >
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+      )}
     </div>
   );
 };
